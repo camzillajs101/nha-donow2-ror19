551a009fb515b34cce07f523380f835bead8c1df
Camilo Mason <camilomason@Camilos-MacBook-Pro.local>
2019-03-15 19:07:50 -0400

    adding stevey harwelly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(name: "Steve Harwell", genre: "rock", age: 52, net_worth: 5000000, num_of_albums: 11)
